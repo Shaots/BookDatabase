0a90def72fedeba95b7aa75c5848eae8ac513401
Roman <roman.miv@yandex.ru>
2025-09-10 15:44:10 +0000

    [a] Add constructor

--- include/book.hpp
+++ include/book.hpp
@@ -1,31 +1,40 @@
 #pragma once
 
+#include <array>
 #include <format>
+#include <iterator>
 #include <stdexcept>
 #include <string_view>
 
 namespace bookdb {
 
-enum class Genre { Fiction, NonFiction, SciFi, Biography, Mystery, Unknown };
+enum class Genre { Fiction, NonFiction, SciFi, Biography, Mystery, Unknown, Count };
 
 // Ваш код для constexpr преобразования строк в enum::Genre и наоборот здесь
 
 constexpr Genre GenreFromString(std::string_view s) {
-    // Ваш код здесь
-    return Genre::Unknown;
+    // Rb tree is not necessary since there are only 6 genres
+    static constexpr std::array<std::string, static_cast<int>(Genre::Count)> arr = {"Fiction", "NonFiction", "SciFi", "Biography", "Mystery", "Unknown"};
+    auto it = std::find(arr.begin(), arr.end(), s);
+    if (it == arr.end())
+        return Genre::Unknown;
+    else
+        return static_cast<Genre>(std::distance(arr.begin(), it));
 }
 
 struct Book {
     // string_view для экономии памяти, чтобы ссылаться на оригинальную строку, хранящуюся в другом контейнере
-    std::string_view author;
-    std::string title;
+    std::string_view author = "";
+    std::string title = "";
 
-    int year;
+    int year = 0;
     Genre genre;
-    double rating;
-    int read_count;
+    double rating = 0.0;
+    int read_count = 0;
 
-    // Ваш код для конструкторов здесь
+    constexpr Book(std::string_view genre_) : genre(GenreFromString(genre_)){}
+
+    constexpr Book(Genre genre_) : genre(genre_){}
 };
 }  // namespace bookdb
 
--- tests/db_test.cpp
+++ tests/db_test.cpp
@@ -1,3 +1,31 @@
 #include <gtest/gtest.h>
+#include "book.hpp"
 
-TEST(TestComponentName, SimpleCheck) { EXPECT_EQ(1 + 1, 2); }
+using namespace bookdb;
+
+
+TEST(TestComponentName, Constructor) { 
+    Book b1(Genre::Fiction);
+    EXPECT_EQ(b1.genre, Genre::Fiction);
+
+    Book b2("Fiction");
+    EXPECT_EQ(b2.genre, Genre::Fiction);
+
+    Book b3("NonFiction");
+    EXPECT_EQ(b3.genre, Genre::NonFiction);
+
+    Book b4("SciFi");
+    EXPECT_EQ(b4.genre, Genre::SciFi);
+
+    Book b5("Biography");
+    EXPECT_EQ(b5.genre, Genre::Biography);
+
+    Book b6("Mystery");
+    EXPECT_EQ(b6.genre, Genre::Mystery);
+
+    Book b7("Unknown");
+    EXPECT_EQ(b7.genre, Genre::Unknown);
+
+    Book b8("ABCDE");
+    EXPECT_EQ(b8.genre, Genre::Unknown);
+}
