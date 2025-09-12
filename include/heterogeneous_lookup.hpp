34224e576eb799d989bbdebcb536a42c40fa7e9c
Roman <roman.miv@yandex.ru>
2025-09-12 15:51:39 +0000

    [a] Add heterogeneous lookup

--- include/heterogeneous_lookup.hpp
+++ include/heterogeneous_lookup.hpp
@@ -1,14 +1,52 @@
 #pragma once
 
+#include <cstddef>
 #include <string>
 #include <string_view>
 
 namespace bookdb {
 
-struct TransparentStringLess {};
+struct TransparentStringLess {
+    using is_transparent = void;
+    bool operator()(const std::string_view str1, const std::string_view str2) {
+        return str1 < str2;
+    }
+    bool operator()(const std::string& str1, const std::string& str2) {
+        return str1 < str2;
+    }
+    bool operator()(const std::string_view str1, const std::string str2) {
+        return str1 < str2;
+    }
+    bool operator()(const std::string& str1, const std::string_view& str2) {
+        return str1 < str2;
+    }
+};
 
-struct TransparentStringEqual {};
+struct TransparentStringEqual {
+    using is_transparent = void;
+    bool operator()(const std::string_view str1, const std::string_view str2) {
+        return str1 == str2;
+    }
+    bool operator()(const std::string& str1, const std::string& str2) {
+        return str1 == str2;
+    }
+    bool operator()(const std::string_view str1, const std::string& str2) {
+        return str1 == str2;
+    }
+    bool operator()(const std::string& str1, const std::string_view str2) {
+        return str1 == str2;
+    }
+};
 
-struct TransparentStringHash {};
+struct TransparentStringHash {
+    using is_transparent = void;
+    size_t operator()(const std::string_view str) {
+        return std::hash<std::string_view>{}(str);
+    }
+
+    size_t operator()(const std::string& str) {
+        return std::hash<std::string>{}(str);
+    }
+};
 
 }  // namespace bookdb
